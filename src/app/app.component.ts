d40796d6887014ed601afd892f6ebb64406ae2eb
George Pagonoudis <giorgospago23@gmail.com>
2020-05-25 20:50:14 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-sockets';
+export class AppComponent implements OnInit {
+
+  public locations = [];
+
+  constructor(private socket: Socket) {}
+
+  ngOnInit() {
+
+    navigator.geolocation.watchPosition(data => {
+
+      const location = {
+        latitude: data.coords.latitude,
+        longitude: data.coords.longitude
+      };
+
+      this.socket.emit("my-location", location);
+
+    })
+
+    this.socket.on("locations", (data) => {
+      this.locations = data;
+    });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SocketIoModule } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyBnUp5T0AiklY5AspJHUO3kn8z1mjJ_KbY"
+    }),
+    SocketIoModule.forRoot({url: "https://api.maps.develobird.com"})
   ],
   providers: [],
   bootstrap: [AppComponent]
